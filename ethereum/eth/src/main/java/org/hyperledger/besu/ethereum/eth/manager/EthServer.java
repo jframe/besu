b208335ef87c4e6c34aa4db10815944b180619e4
Jason Frame <jason.frame@consensys.net>
2024-06-24 13:33:10 +1000

    Log getHeaders params
    Signed-off-by: Jason Frame <jason.frame@consensys.net>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/EthServer.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/EthServer.java
@@ -128,7 +128,13 @@ class EthServer {
     final int skip = getHeaders.skip();
     final int maxHeaders = Math.min(requestLimit, getHeaders.maxHeaders());
     final boolean reversed = getHeaders.reverse();
-    LOG.debug("Processing GetBlockHeaders request: hash {}, blockNumber {}, skip {}, maxHeaders {}, reversed {}", hash, getHeaders.blockNumber(), skip, getHeaders.maxHeaders(), reversed);
+    LOG.debug(
+        "Processing GetBlockHeaders request: hash {}, blockNumber {}, skip {}, maxHeaders {}, reversed {}",
+        hash,
+        getHeaders.blockNumber(),
+        skip,
+        getHeaders.maxHeaders(),
+        reversed);
     final BlockHeader firstHeader;
     // Query first header by hash or number depending on request arguments
     if (hash.isPresent()) {
