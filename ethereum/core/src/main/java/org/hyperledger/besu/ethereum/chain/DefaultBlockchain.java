dec33ac9d980945a2a9d123ef22239c8dddb5fe8
Jason Frame <jason.frame@consensys.net>
2024-09-20 14:18:57 +1000

    update ttd and chain head when storing block
    Signed-off-by: Jason Frame <jason.frame@consensys.net>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/chain/DefaultBlockchain.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/chain/DefaultBlockchain.java
@@ -433,11 +433,16 @@ public class DefaultBlockchain implements MutableBlockchain {
     if (numberOfBlocksToCache != 0) cacheBlockData(block, receipts);
     final BlockchainStorage.Updater updater = blockchainStorage.updater();
     final Hash hash = block.getHash();
+    final Difficulty totalDifficulty = calculateTotalDifficulty(block.getHeader());
     updater.putBlockHeader(hash, block.getHeader());
     updater.putBlockHash(block.getHeader().getNumber(), hash);
     updater.putBlockBody(hash, block.getBody());
     updater.putTransactionReceipts(hash, receipts);
+    updater.putTotalDifficulty(hash, totalDifficulty);
+    updater.setChainHead(hash);
     updater.commit();
+    this.chainHeader = block.getHeader();
+    this.totalDifficulty = totalDifficulty;
   }
 
   private void cacheBlockData(final Block block, final List<TransactionReceipt> receipts) {
