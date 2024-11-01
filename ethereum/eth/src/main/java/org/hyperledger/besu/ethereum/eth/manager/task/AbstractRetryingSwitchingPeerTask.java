e1955a622ef069ea2412d75c30405d1075244ee7
Jason Frame <jason.frame@consensys.net>
2024-11-01 12:38:56 +1000

    change RetryingSwitchingPeerTask to sort by least to most busy same as PendingPeerRequest
    Signed-off-by: Jason Frame <jason.frame@consensys.net>

--- ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/task/AbstractRetryingSwitchingPeerTask.java
+++ ethereum/eth/src/main/java/org/hyperledger/besu/ethereum/eth/manager/task/AbstractRetryingSwitchingPeerTask.java
@@ -131,7 +131,8 @@ public abstract class AbstractRetryingSwitchingPeerTask<T> extends AbstractRetry
         .getEthPeers()
         .streamBestPeers()
         .filter(this::isSuitablePeer)
-        .filter(peer -> !triedPeers.contains(peer));
+        .filter(peer -> !triedPeers.contains(peer))
+        .sorted(EthPeers.LEAST_TO_MOST_BUSY);
   }
 
   private void refreshPeers() {
