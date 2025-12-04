f4f4e5a1dc82856383f66d84375000591f03eac7
Jason Frame <jason.frame@consensys.net>
2025-12-04 17:49:52 +1000

    migrate accounts and storage in parallel
    Signed-off-by: Jason Frame <jason.frame@consensys.net>

--- ethereum/core/src/test/java/org/hyperledger/besu/ethereum/trie/pathbased/bonsai/worldview/BonsaiArchiveFlatDbMigratorTest.java
+++ ethereum/core/src/test/java/org/hyperledger/besu/ethereum/trie/pathbased/bonsai/worldview/BonsaiArchiveFlatDbMigratorTest.java
@@ -84,7 +84,7 @@ class BonsaiArchiveFlatDbMigratorTest {
 
     blockchain = createBlockchain();
 
-    executorService = Executors.newScheduledThreadPool(1);
+    executorService = Executors.newScheduledThreadPool(2); // Use 2 threads for parallel processing
 
     migrator =
         new BonsaiArchiveFlatDbMigrator(
