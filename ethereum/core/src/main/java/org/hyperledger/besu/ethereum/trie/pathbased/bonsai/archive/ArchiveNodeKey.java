ea14899acde4af47de5793d0c0832d63a56e7a05
Jason Frame <jason.frame@consensys.net>
2026-08-03 16:49:23 +1000

    refactor(bonsai-archive): move ArchiveNodeKey into bonsai.archive package
    Signed-off-by: Jason Frame <jason.frame@consensys.net>

--- ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/bonsai/archive/ArchiveNodeKey.java
+++ ethereum/core/src/main/java/org/hyperledger/besu/ethereum/trie/pathbased/bonsai/archive/ArchiveNodeKey.java
@@ -12,7 +12,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-package org.hyperledger.besu.ethereum.trie.pathbased.bonsai.storage.triehistory;
+package org.hyperledger.besu.ethereum.trie.pathbased.bonsai.archive;
 
 import org.apache.tuweni.bytes.Bytes;
 
--- ethereum/core/src/test/java/org/hyperledger/besu/ethereum/trie/pathbased/bonsai/archive/ArchiveNodeKeyTest.java
+++ ethereum/core/src/test/java/org/hyperledger/besu/ethereum/trie/pathbased/bonsai/archive/ArchiveNodeKeyTest.java
@@ -12,7 +12,7 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-package org.hyperledger.besu.ethereum.trie.pathbased.bonsai.storage.triehistory;
+package org.hyperledger.besu.ethereum.trie.pathbased.bonsai.archive;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
